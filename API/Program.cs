5c275f81c733ba6e2c7d7f285eb08a8f0024e6a1
thinga <32902065+thinga@users.noreply.github.com>
2021-09-14 22:19:35 +0200

    3/28. Adding Seed data

--- API/Program.cs
+++ API/Program.cs
@@ -23,6 +23,7 @@ namespace API
                 {
                     var context = Services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
+                    await StoreContextSeed.SeedAsync(context, loggerFactory);
                 }
                 catch (Exception ex)
                 {
