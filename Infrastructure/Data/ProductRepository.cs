0f460dcb35e292fde98c27a41e20890ac94f5af6
thinga <32902065+thinga@users.noreply.github.com>
2021-09-15 11:23:26 +0200

    4/36. Creating a specification class

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
@@ -23,7 +24,7 @@ namespace Infrastructure.Data
         }
 
         public async  Task<IReadOnlyList<Product>> GetProductsAsync()
-        {
+        { 
             return await _context.Products
                 .Include(p => p.ProductBrand)
                 .Include(p => p.ProductType)
