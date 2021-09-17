69f7591dac46314795fc91501b9de0b9c437b20d
thinga <32902065+thinga@users.noreply.github.com>
2021-09-17 11:37:13 +0200

    6/63. Adding Pagination Part 1

--- Core/Specifications/ISpecification.cs
+++ Core/Specifications/ISpecification.cs
@@ -13,6 +13,8 @@ namespace Core.Specifications
 
         Expression<Func<T, object>> OrderByDescending {get; }
 
-         
+        int Take {get;}
+        int Skip {get;}
+        bool IsPagingEnabled {get;}       
     }
 }
