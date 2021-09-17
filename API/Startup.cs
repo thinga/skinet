2da45f375771bbdafd68b97ba6c5c16093eb33dd
thinga <32902065+thinga@users.noreply.github.com>
2021-09-17 17:21:30 +0200

    6/67. Adding CORS Support to the API (End of Section 6)

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,14 @@ namespace API
            
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
+
        
         }
 
@@ -50,6 +58,8 @@ namespace API
             app.UseRouting();
             
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
