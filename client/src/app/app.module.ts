28a00e011a07418e6fc9d91c4b9e2aad2b6e4644
thinga <32902065+thinga@users.noreply.github.com>
2021-09-24 09:20:53 +0200

    116. Creating an error component for testing the error responses + 117.additional error components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,28 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import  {HttpClientModule} from '@angular/common/http'
-import { CoreModule } from './core/nav-bar/core.module';
-import { ShopModule } from './shop/shop.module';
-
+import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-   
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
     HomeModule
   ],
   providers: [],
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -2,12 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 
-
-
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
+  declarations: [HomeComponent],
   imports: [
     CommonModule
   ],
