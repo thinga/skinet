5d5b4a3dbda912489e06bc6f3a200b2660745b1d
thinga <32902065+thinga@users.noreply.github.com>
2021-10-01 09:43:52 +0200

    15/168. Adding identity to program class+169. Adding an Account controller

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,11 @@
 
 using System;
 using System.Threading.Tasks;
+using Core.Entities.Identity;
 using Infrastructure.Data;
+using Infrastructure.Identity;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +27,12 @@ namespace API
                     var context = Services.GetRequiredService<StoreContext>();
                     await context.Database.MigrateAsync();
                     await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+                    var userManager = Services.GetRequiredService<UserManager<AppUser>>();
+                    var identityContext = Services.GetRequiredService<AppIdentityDbContext>();
+                    await identityContext.Database.MigrateAsync();
+                    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
+
                 }
                 catch (Exception ex)
                 {
