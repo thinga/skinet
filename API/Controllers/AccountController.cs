7a74a56c64259555b5f2f23b430caecc898c33e9
thinga <32902065+thinga@users.noreply.github.com>
2021-10-04 21:20:38 +0200

    15/177. Adding another Dto for the user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using API.Dtos;
 using API.Errors;
 using API.Extensions;
+using AutoMapper;
 using Core.Entities.Identity;
 using Core.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -17,49 +18,68 @@ namespace API.Controllers
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokenService;
+          private readonly IMapper _mapper;
         public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
-        
-        ITokenService tokenService)
+      
+        ITokenService tokenService, IMapper mapper )
         {
+            _mapper = mapper;
             _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
         }
 
-        [Authorize]
-        [HttpGet]
-        public async Task<ActionResult<UserDto>> GetCurrentUser()
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<UserDto>> GetCurrentUser()
+    {
+
+        // var email = User.FindFristValue(ClaimTypes.Email);
+
+        var user = await _userManager.FindByEmailFromClaimsPrinciple(HttpContext.User);
+        // var user = await _userManager.FindByEmailFromClaimsPrinciple(User);
+
+        return new UserDto
         {
-          
-            // var email = User.FindFristValue(ClaimTypes.Email);
+            Email = user.Email,
+            Token = _tokenService.CreateToken(user),
+            DisplayName = user.DisplayName
+        };
+    }
 
-            var user = await _userManager.FindByEmailFromClaimsPrinciple(HttpContext.User);
-            // var user = await _userManager.FindByEmailFromClaimsPrinciple(User);
+    [HttpGet("emailexists")]
+    public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
+    {
+        return await _userManager.FindByEmailAsync(email) != null;
+    }
 
-            return new UserDto 
-            {
-                Email = user.Email,
-                Token = _tokenService.CreateToken(user),
-                DisplayName = user.DisplayName
-            };
-        }
+    [Authorize]
+    [HttpGet("address")]
+    public async Task<ActionResult<AddressDto>> GetUserAddress()
+    {
 
-        [HttpGet("emailexists")]
-        public async Task<ActionResult<bool>> CheckEmailExistsAsync([FromQuery] string email)
-        {
-            return await _userManager.FindByEmailAsync(email) != null;
-        }
-        
-        [Authorize]
-        [HttpGet("address")]
-        public async Task<ActionResult<Address>> GetUserAddress()
-        {
+        var user = await _userManager.FindUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
 
-            var user = await _userManager.FindUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
+        return _mapper.Map<Address, AddressDto>(user.Address);
 
-            return user.Address;
+    }
+    [Authorize]
+    [HttpPut("address")]
 
-        }
+    public async Task<ActionResult<AddressDto>> UppdateUserAddress(AddressDto address)
+    {
+        var user = await _userManager.FindUserByClaimsPrincipleWithAddressAsync(HttpContext.User);
+
+        user.Address = _mapper.Map<AddressDto, Address>(address);
+
+        var result = await _userManager.UpdateAsync(user);
+
+        if (result.Succeeded) return Ok(_mapper.Map<Address, AddressDto>(user.Address));
+
+        return BadRequest("Problem updating the user");
+
+
+    }
 
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
---
+++ API/Dtos/AddressDto.cs
@@ -0,0 +1,12 @@
+namespace API.Dtos
+{
+    public class AddressDto
+    {
+         public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+         public string ZipCode { get; set; }
+    }
+}
