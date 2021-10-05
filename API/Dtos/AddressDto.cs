93986c23a1834524f884eeb6b225b92fdf360bf1
thinga <32902065+thinga@users.noreply.github.com>
2021-10-05 15:32:31 +0200

    16/181.Validation+182. Checking for duplicate email addresses

--- API/Dtos/AddressDto.cs
+++ API/Dtos/AddressDto.cs
@@ -1,12 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class AddressDto
     {
+        [Required]
          public string Firstname { get; set; }
+         
+         [Required]
         public string Lastname { get; set; }
+
+        [Required]
         public string Street { get; set; }
+
+        [Required]
         public string City { get; set; }
+
+        [Required]
         public string State { get; set; }
+
+        [Required]
          public string ZipCode { get; set; }
     }
 }
