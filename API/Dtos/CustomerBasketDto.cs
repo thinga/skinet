884a810c65ede199b1f01e3e4ec92b513d1afb4f
thinga <32902065+thinga@users.noreply.github.com>
2021-10-05 16:01:49 +0200

    16/183. Validating the basket

---
+++ API/Dtos/CustomerBasketDto.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Core.Entities;
+
+namespace API.Dtos
+{
+    public class CustomerBasketDto
+    {
+        [Required]
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; }
+        
+    }
+}
