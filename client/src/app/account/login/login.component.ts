ae8d4d80cbf70948bed24fba55bb1258f75854df
thinga <32902065+thinga@users.noreply.github.com>
2021-10-07 19:09:33 +0200

    17/204. Creating an auth guard

--- client/src/app/account/login/login.component.ts
+++ client/src/app/account/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { AccountRoutingModule } from '../account-routing.module';
 import { AccountService } from '../account.service';
 
@@ -11,10 +11,13 @@ import { AccountService } from '../account.service';
 })
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
+  returnUrl: string;
 
-  constructor(private accountService: AccountService , private router: Router ) { }
 
-  ngOnInit(): void {
+  constructor(private accountService: AccountService , private router: Router, private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.returnUrl = this.activatedRoute.snapshot.queryParams.returnUrl || '/shop';
     this.createLoginForm();
   }
 
@@ -27,7 +30,7 @@ export class LoginComponent implements OnInit {
 
   onSubmit() {
     this.accountService.login(this.loginForm.value).subscribe(() => {
-      this.router.navigateByUrl('/shop');
+      this.router.navigateByUrl(this.returnUrl);
     }, error => {
       console.log(error);
     });
