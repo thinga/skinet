26d1d1f911ac2972ab595f762d928782aa06527a
thinga <32902065+thinga@users.noreply.github.com>
2021-10-07 21:32:54 +0200

    17/205. Using the replay subject

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -23,14 +23,13 @@ export class AppComponent implements OnInit {
 
 loadCurrentUser() {
   const token = localStorage.getItem('token');
-  if (token) {
     this.accountService.loadCurrentUser(token).subscribe(() => {
       console.log('loaded user');
     }, error => {
       console.log(error);
     });
   }
-}
+
 
 loadBasket() {
   const basketId = localStorage.getItem('basket_id');
