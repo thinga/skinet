3518cc124f92a864e2331a6a75b8ecc63ff0e391
thinga <32902065+thinga@users.noreply.github.com>
2021-10-08 17:44:02 +0200

    18/214. Implementing the create order method

--- Infrastructure/Services/OrderService.cs
+++ Infrastructure/Services/OrderService.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
 using Core.Entities.OrderAggregte;
 using Core.Interfaces;
 
@@ -7,24 +9,71 @@ namespace Infrastructure.Services
 {
     public class OrderService : IOrderService
     {
-        public Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethod, string basketId, Address shippingAddress)
+        private readonly IGenericRepository<Order> _orderRepo;
+        private readonly IGenericRepository<DeliveryMethod> _dmRepo;
+          private readonly IGenericRepository<Product> _productRepo;
+        private readonly IBasketRepository _basketRepo;
+        public OrderService(IGenericRepository<Order> orderRepo, IGenericRepository<DeliveryMethod> dmRepo,
+      
+        IGenericRepository<Product> productRepo, IBasketRepository basketRepo)
         {
-            throw new System.NotImplementedException();
+            _basketRepo = basketRepo;
+            _productRepo = productRepo;
+            _dmRepo = dmRepo;
+            _orderRepo = orderRepo;
         }
 
-        public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+    public async Task<Order> CreateOrderAsync(string buyerEmail, int deliveryMethodId, string basketId, Address shippingAddress)
+    {
+        // get basket from the repo
+        var basket = await _basketRepo.GetBasketAsync(basketId);
+
+        // get items from the product repo
+        var items = new List<OrderItem>();
+        foreach (var item in basket.Items)
         {
-            throw new System.NotImplementedException();
+            var productItem = await _productRepo.GetByIdAsync(item.Id);
+            var itemOrdered = new ProductItemOrdered(productItem.Id, productItem.Name,
+            productItem.PictureUrl);
+            var orderItem = new OrderItem(itemOrdered, productItem.Price, item.Quantity);
+            items.Add(orderItem);
+
         }
 
-        public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
-        {
-            throw new System.NotImplementedException();
-        }
+        // get delivery method from repo
+        var deliveryMethod = await _dmRepo.GetByIdAsync(deliveryMethodId);
 
-        public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
-        {
-            throw new System.NotImplementedException();
-        }
+
+        // calc subtotal
+        var subtotal = items.Sum(items => items.Price * items.Quantity);
+
+
+        // create order
+        var order = new Order(items, buyerEmail, shippingAddress, deliveryMethod, subtotal);
+
+
+       //TODO: save to db
+
+       // return order
+       return order;
+
+       
+
     }
+
+    public Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<Order> GetOrderByIdAsync(int id, string buyerEmail)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail)
+    {
+        throw new System.NotImplementedException();
+    }
+}
 }
