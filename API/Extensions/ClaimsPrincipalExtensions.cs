48b9a0ec79069041446e21cbaf7599431e63b172
thinga <32902065+thinga@users.noreply.github.com>
2021-10-08 20:40:19 +0200

    18/215. Creating the order controller

---
+++ API/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
+using Core.Entities.OrderAggregte;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string RetrieveEmailFromPrincipal(this ClaimsPrincipal user)
+        {
+            // return user.FindirstValue(ClaimTypes.Email);
+            return user?.Claims?.FirstOrDefault(xas => xas.Type == ClaimTypes.Email)?.Value;
+
+        }
+
+    }
+}
