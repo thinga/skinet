41b451a5f9dafa430d02ed94076234fc7527d6cf
thinga <32902065+thinga@users.noreply.github.com>
2021-10-22 20:42:50 +0200

    261. Creating the payment controller

--- Infrastructure/Services/PaymentService.cs
+++ Infrastructure/Services/PaymentService.cs
@@ -23,58 +23,60 @@ namespace Infrastructure.Services
             _basketRepository = basketRepository;
         }
 
-    public async  Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
-    {
-        StripeConfiguration.ApiKey = _config["StripeSettings: SecretKey"];
-        var basket = await _basketRepository.GetBasketAsync(basketId);
-        var shippingPrice = 0m;
-
-        if (basket.DeliveryMethodId.HasValue)
+    public async Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
         {
-            var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>()
-                .GetByIdAsync((int)basket.DeliveryMethodId);
+            StripeConfiguration.ApiKey = _config["StripeSettings:SecretKey"];
+
+            var basket = await _basketRepository.GetBasketAsync(basketId);
+
+            if (basket == null) return null;
+
+            var shippingPrice = 0m;
+
+            if (basket.DeliveryMethodId.HasValue)
+            {
+                var deliveryMethod = await _unitOfWork.Repository<DeliveryMethod>().GetByIdAsync((int)basket.DeliveryMethodId);
                 shippingPrice = deliveryMethod.Price;
-        }
-
-        foreach (var item in basket.Items)
-        {
-            var productItem = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
-            if (item.Price != productItem.Price)
-            {
-                item.Price = productItem.Price;
             }
-        }
 
-        var service = new PaymentIntentService();
-        PaymentIntent intent;
-
-        if (string.IsNullOrEmpty(basket.PaymentIntentId))
-        {
-            var options = new PaymentIntentCreateOptions
+            foreach (var item in basket.Items)
             {
-                Amount = (long) basket.Items.Sum(intent => intent.Quantity * (intent.Price * 100)) + (long)
-                shippingPrice * 100,
-                Currency = "usd",
-                PaymentMethodTypes = new List<string> {"card"}
-            };
-            intent = await service.CreateAsync(options);
-            basket.PaymentIntentId = intent.Id;
-            basket.ClientSecret = intent.ClientSecret;
-        }
-        else
-        {
-            var options = new PaymentIntentUpdateOptions
+                var productItem = await _unitOfWork.Repository<Product>().GetByIdAsync(item.Id);
+                if (item.Price != productItem.Price)
+                {
+                    item.Price = productItem.Price;
+                }
+            }
+
+            var service = new PaymentIntentService();
+
+            PaymentIntent intent;
+
+            if (string.IsNullOrEmpty(basket.PaymentIntentId))
             {
-                Amount = (long) basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)
-                shippingPrice * 100
-            };
-            await service.UpdateAsync(basket.PaymentIntentId, options);
-            
+                var options = new PaymentIntentCreateOptions
+                {
+                    Amount = (long)basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)shippingPrice * 100,
+                    Currency = "usd",
+                    PaymentMethodTypes = new List<string> { "card" }
+                };
+                intent = await service.CreateAsync(options);
+                basket.PaymentIntentId = intent.Id;
+                basket.ClientSecret = intent.ClientSecret;
+            }
+            else
+            {
+                var options = new PaymentIntentUpdateOptions
+                {
+                    Amount = (long)basket.Items.Sum(i => i.Quantity * (i.Price * 100)) + (long)shippingPrice * 100
+                };
+                await service.UpdateAsync(basket.PaymentIntentId, options);
+            }
+
+            await _basketRepository.UpdateBasketAsync(basket);
+
+            return basket;
         }
-
-        await _basketRepository.UpdateBasketAsync(basket);
-
-        return basket;
     }
-}
+
 }
