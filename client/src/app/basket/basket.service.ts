a8061fb90f7fa337f048b959da578a5d632add36
thinga <32902065+thinga@users.noreply.github.com>
2021-10-22 22:13:22 +0200

    21/265. Implementing stripe elements

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -26,7 +26,6 @@ export class BasketService {
     .pipe(
       map((basket: IBasket) => {
         this.basketSource.next(basket);
-        console.log(this.getCurrentBasketValue());
       })
     );
   }
