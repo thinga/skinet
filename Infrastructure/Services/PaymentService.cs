54b2cf118a10ce3be2077bf8fcd83e927f025585
thinga <32902065+thinga@users.noreply.github.com>
2021-10-22 16:12:06 +0200

    21/259. Creating the payment service and interface

---
+++ Infrastructure/Services/PaymentService.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using Core.Entities;
+using Core.Interfaces;
+
+namespace Infrastructure.Services
+{
+    public class PaymentService : IPaymentService
+    {
+       
+        public Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
