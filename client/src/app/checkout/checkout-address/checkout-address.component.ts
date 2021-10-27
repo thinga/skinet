de74d2f9f056304bc504b2564b4db2dc0fbc9901
thinga <32902065+thinga@users.noreply.github.com>
2021-10-27 11:03:28 +0200

    21/274. Enabling linear mode

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from 'src/app/account/account.service';
+import { IAddress } from 'src/app/shared/models/address';
 
 @Component({
   selector: 'app-checkout-address',
@@ -17,8 +18,9 @@ export class CheckoutAddressComponent implements OnInit {
 
   saveUserAddress() {
     this.accountService.updateUserAddress(this.checkoutForm.get('addressForm').value)
-         .subscribe(() => {
+         .subscribe((address: IAddress) => {
            this.toastr.success('Address saved');
+           this.checkoutForm.get('addressorm').reset(address);
          }, error => {
            this.toastr.error(error.message);
            console.log(error);
