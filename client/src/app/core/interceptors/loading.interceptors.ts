6d8723a9b9b624f99a032f73d7009fb19d1e310e
thinga <32902065+thinga@users.noreply.github.com>
2021-10-27 10:31:33 +0200

    21/273. Loading indicators

--- client/src/app/core/interceptors/loading.interceptors.ts
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -9,6 +9,9 @@ export class LoadingInterceptor implements HttpInterceptor {
     constructor(private busyService: BusyService) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (req.method === 'POST' && req.url.includes('order')) {
+            return next.handle(req);
+        }
        if (!req.url.includes('emailexists')) {
            this.busyService.busy();
        }
