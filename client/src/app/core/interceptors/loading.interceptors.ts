cd716d9e33f1d914b3ff43508c02d0255a89a2ec
thinga <32902065+thinga@users.noreply.github.com>
2021-11-06 16:36:10 +0100

    SKINET5: 287. Pre-publishing adjustments

--- client/src/app/core/interceptors/loading.interceptors.ts
+++ client/src/app/core/interceptors/loading.interceptors.ts
@@ -12,6 +12,9 @@ export class LoadingInterceptor implements HttpInterceptor {
         if (req.method === 'POST' && req.url.includes('order')) {
             return next.handle(req);
         }
+        if (req.method === 'DELETE') {
+            return next.handle(req);
+        }
        if (!req.url.includes('emailexists')) {
            this.busyService.busy();
        }
